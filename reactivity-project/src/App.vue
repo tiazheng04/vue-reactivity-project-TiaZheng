4c45d94fbf79bbf028bbc4b48a6baeb28058454a
TiaZheng <71717366+TiaZheng@users.noreply.github.com>
2021-10-27 13:38:17 -0400

    flexbox things

--- reactivity-project/src/App.vue
+++ reactivity-project/src/App.vue
@@ -3,9 +3,18 @@
     <HelloWorld msg="Build your outfit"/>
     <img class="main-img" alt="Vue logo" src="./assets/main.jpg">
     <div class= "column-container">
-      <div class="column column 1">Tops</div>
-      <div class="column column 2">xxx</div>
-      <div class="column column 3">Footwear</div>  
+      <div class="column column 1">
+        <h2 class="section-name">Tops</h2>
+        <h3 class="lists">lists</h3>
+      </div>
+      <div class="column column 1">
+        <h2 class="section-name">xxx</h2>
+        <h3 class="lists">lists</h3>
+      </div>
+      <div class="column column 1">
+        <h2 class="section-name">Footwear</h2>
+        <h3 class="lists">lists</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -28,21 +37,44 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   font-size: 62.5%;
-  background-color: ;
+  background-color: rgb(160, 193, 236);
+  height: 100vh;
 }
 .main-img{
-  width: 80vw;
-  margin: 2rem 0rem 4rem
+  width: 50vw;
+  margin: 2rem 0rem 4rem;
+  border-radius: 1rem;
 }
 .column-container{
   display:flex;
   flex-direction: row;
-  width:100vw;
+  width:90vw;
   justify-content: space-around;
+  background-color: white;
+  height: 40vh;
 }
+.section-name{
+  border: solid;
+  border-width: .1rem;
+}
+
 .column{
-  width: 17rem
+  width: 15rem;
+  height: 35vh;
+  
 }
+
+.lists{
+  border-left: dashed;
+  border-width: .1rem;
+  background-color: blue;
+  height: 20rem;
+  width: 14rem;
+}
+
 </style>
